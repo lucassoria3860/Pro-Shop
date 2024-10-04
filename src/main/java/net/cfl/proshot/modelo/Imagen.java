d5f9d608a31115712e579d173ca7827dd6c4f418
Programacion7 <Programacion7@cfl414.com>
2024-10-04 11:38:31 -0300

    creadas las clases de JPA Producto, Categoria e Imagen. Modificado el Aplication Properties

---
+++ src/main/java/net/cfl/proshot/modelo/Imagen.java
@@ -0,0 +1,37 @@
+package net.cfl.proshot.modelo;
+
+import java.sql.Blob;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Imagen {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String archivoNombre;
+	private String achivoTipo;
+	
+	@Lob
+	private Blob imagen;
+	private String descargaUrl;
+	
+	@ManyToOne
+	@JoinColumn(name="producto_id")
+	private Producto producto;
+	
+}
