4b43b6ae97972cbc5d64855e0a1fa84e65d3e526
Programacion7 <Programacion7@cfl414.com>
2024-10-04 10:32:52 -0300

    commit inicial

---
+++ src/main/java/net/cfl/proshot/ProShotApplication.java
@@ -0,0 +1,13 @@
+package net.cfl.proshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProShotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProShotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cfl/proshot/ProShotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cfl.proshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProShotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
