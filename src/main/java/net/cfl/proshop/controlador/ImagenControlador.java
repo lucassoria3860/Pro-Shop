3c94993962518afd5f9d338385c4cc0bdbe449e0
Programacion7 <Programacion7@cfl414.com>
2024-10-18 11:19:13 -0300

    implementado el metodo categoriaControlador

--- src/main/java/net/cfl/proshop/controlador/ImagenControlador.java
+++ src/main/java/net/cfl/proshop/controlador/ImagenControlador.java
@@ -9,9 +9,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,7 @@ import net.cfl.proshop.dto.ImagenDto;
 import net.cfl.proshop.modelo.Imagen;
 import net.cfl.proshop.respuesta.ApiRespuesta;
 import net.cfl.proshop.servicio.imagen.IImagenServicio;
+import net.cfl.proshop.servicio.imagen.ImagenServicio;
 
 @RequiredArgsConstructor
 @RestController
@@ -37,7 +41,8 @@ public class ImagenControlador {
 			List<ImagenDto> imagenesDto = imagenServicio.guardaImagenes(archivos, idPorducto);
 			return ResponseEntity.ok(new ApiRespuesta("imagen subida correctamente", imagenesDto));
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiRespuesta("Error al subir imagen", e.getMessage()));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+						.body(new ApiRespuesta("Error al subir imagen", e.getMessage()));
 		}
 		
 	}
@@ -49,4 +54,40 @@ public class ImagenControlador {
 					.header(HttpHeaders.CONTENT_DISPOSITION, "Attachment: filename=\"" + imagen.getArchivoNombre() + "\"")
 					.body(recurso);
 		}
+	
+	@PutMapping("/imagen/{imagenId}/actualiza")
+	public ResponseEntity<ApiRespuesta> actualizaImagen(@PathVariable Long imagenId, @RequestBody MultipartFile archivo){
+		try {
+			Imagen imagen = imagenServicio.listaImagenPorId(imagenId) ;
+			if(imagen != null) {
+				imagenServicio.actualizarImagen(archivo, imagenId);
+				return ResponseEntity.ok(new ApiRespuesta("imagen actualizada", null));
+			}
+		} catch (Exception e) {
+			ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiRespuesta(e.getMessage(),null ));
+		}
+		
+			
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta("Fallo al actrualizar la imagen", null));
+	}
+	
+	
+	@DeleteMapping("/imagen/{imagenId}/borrar")
+	public ResponseEntity<ApiRespuesta> borraImagen(@PathVariable Long imagenId){
+		try {
+			Imagen imagen = imagenServicio.listaImagenPorId(imagenId);
+			if(imagen != null) {
+				imagenServicio.borraImagenPorId(imagenId);
+				return ResponseEntity.ok(new ApiRespuesta("imagen Borrada", null));
+			}
+		} catch (Exception e) {
+			ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(new ApiRespuesta(e.getMessage(),null ));
+		}
+		
+			
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta("Fallo al actrualizar la imagen", null));
+	}
 }
