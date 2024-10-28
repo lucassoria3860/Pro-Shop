fed5073d2b8ce821304dec9a1154831fb3a061ab
Lucas Soria <lucassoria624@gmail.com>
2024-10-27 23:39:48 -0300

    creados los atributos de las clases Carrito y CarritoItem

--- src/main/java/net/cfl/proshop/modelo/Carrito.java
+++ src/main/java/net/cfl/proshop/modelo/Carrito.java
@@ -1,5 +1,23 @@
 package net.cfl.proshop.modelo;
 
+import java.math.BigDecimal;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Carrito {
 
+	private Long id;
+	private BigDecimal montoTotal;
+	private Set item;
+	
 }
--- src/main/java/net/cfl/proshop/modelo/CarritoItem.java
+++ src/main/java/net/cfl/proshop/modelo/CarritoItem.java
@@ -1,5 +1,24 @@
 package net.cfl.proshop.modelo;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class CarritoItem {
 
+	private String producto;
+	private int catnidad;
+	private BigDecimal presioUni;
+	private BigDecimal precioTot;
+	private Carrito carrito;
+	
 }
