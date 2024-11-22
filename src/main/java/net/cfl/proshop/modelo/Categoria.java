46325e4fbe45de7c540fbfbfef9b9559b5e803ef
Programacion7 <Programacion7@cfl414.com>
2024-11-22 11:37:45 -0300

    En la clase de hoy, cambiamos los nombres a las tablas de las bases de datos, en las clases Producto y Categoria

--- src/main/java/net/cfl/proshop/modelo/Categoria.java
+++ src/main/java/net/cfl/proshop/modelo/Categoria.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +22,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "categorias")
 public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "cat_nombre")
 	private String nombre;
 	
 	@JsonIgnore
--- src/main/java/net/cfl/proshop/modelo/Producto.java
+++ src/main/java/net/cfl/proshop/modelo/Producto.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "productos")
 public class Producto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
